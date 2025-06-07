1cbc81c8b8f59b83816ca01b3f13441cca8080a1
Syracusa <msrndsy@gmail.com>
2025-06-07 10:31:56 +0900

    Game size for vertical screen

--- src/game/main.js
+++ src/game/main.js
@@ -9,8 +9,8 @@ import { AUTO, Game } from 'phaser';
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 768,
+    height: 1024,
     parent: 'game-container',
     backgroundColor: '#028af8',
     scale: {
