1f3a428f0a4b04e108979cf7c4d3e744f7ada167
Syracusa <msrndsy@gmail.com>
2025-06-08 15:04:39 +0900

    Main & Gameover style adjust

--- src/game/scenes/GameOver.js
+++ src/game/scenes/GameOver.js
@@ -1,5 +1,6 @@
 import { Scene } from 'phaser';
 import { screenDef } from '../../def';
+import { createGradientBackground } from '../utils/graphicsUtils';
 
 export class GameOver extends Scene
 {
@@ -10,9 +11,11 @@ export class GameOver extends Scene
 
     create ()
     {
-        this.cameras.main.setBackgroundColor(0xff0000);
+        // this.cameras.main.setBackgroundColor(0xff0000);
+        // this.add.image(512, 384, 'background').setAlpha(0.5);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+        const randomPalette = { start: 0x8ec5fc, end: 0xe0c3fc };
+        createGradientBackground(this, randomPalette.start, randomPalette.end);
 
         this.add.text(screenDef.width / 2, 384, 'Game Over', {
             fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,5 +1,6 @@
 import { Scene } from 'phaser';
 import { screenDef } from '../../def';
+import { createGradientBackground } from '../utils/graphicsUtils';
 
 export class MainMenu extends Scene {
     constructor() {
@@ -7,7 +8,9 @@ export class MainMenu extends Scene {
     }
 
     create() {
-        this.add.image(512, 384, 'background');
+        // this.add.image(512, 384, 'background');
+        const randomPalette = { start: 0x8ec5fc, end: 0xe0c3fc };
+        createGradientBackground(this, randomPalette.start, randomPalette.end);
 
         this.add.text(screenDef.width / 2, 300, 'Block Breaker', {
             fontFamily: 'Arial Black', fontSize: 50, color: '#ffffff',
@@ -15,7 +18,7 @@ export class MainMenu extends Scene {
             align: 'center'
         }).setOrigin(0.5);
 
-        this.add.text(screenDef.width / 2, 460, 'Main Menu', {
+        this.add.text(screenDef.width / 2, 460, '아무곳이나 클릭하여 시작', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
